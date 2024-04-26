d140c33304c679556b37525cbea6cf3b0d608d89
thaisiporto <thaisi.porto@gmail.com>
2024-04-26 06:39:30 -0300

    Update script-layout.js

--- scripts/script-layout.js
+++ scripts/script-layout.js
@@ -1,15 +1,22 @@
 function clickMenu() {
-    if (menuOpen.style.display == 'block') {
-        menuOpen.style.display = 'none'
+    var iconeB = window.document.querySelector("i#burgerMenu");
+    var iconeX = window.document.querySelector("i#closeMenu");
+
+    if (menuOpen.style.display == "block") {
+        menuOpen.style.display = "none";
+        iconeX.style.display = "none"
+        iconeB.style.display = "block"
     } else {
-        menuOpen.style.display = 'block';
+        menuOpen.style.display = "block";
+        iconeB.style.display = "none"
+        iconeX.style.display = "block"
     }
 }
 
 function showMenu() {
     if (window.innerWidth >= 700) {
-        menuOpen.style.display = 'block'
+        menuOpen.style.display = "block";
     } else {
-        menuOpen.style.display = 'none'
+        menuOpen.style.display = "none";
     }
 }
