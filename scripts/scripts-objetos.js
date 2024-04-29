95e140ecb460481ce0e2f5d5b5633f8cce73236e
thaisiporto <thaisi.porto@gmail.com>
2024-04-29 05:27:04 -0300

    Create scripts-objetos.js

---
+++ scripts/scripts-objetos.js
@@ -0,0 +1,21 @@
+function pizSal() {
+    let pizza = {  
+        sabor: "calabresa",  
+        tamanho:"grande",  
+        borda: "catupiry"  
+    };
+    let pedido = window.document.querySelector("p#pizzaSalgada")
+
+    pedido.innerHTML = `Gostaria de pedir uma pizza <u>${pizza.tamanho}</u> de <u>${pizza.sabor}</u> com borda de <u>${pizza.borda}</u>.`
+}
+
+function pizDoc() {
+    let pizza = new Object();  
+    pizza.sabor = "brigadeiro";  
+    pizza.tamanho = "broto";  
+    pizza.borda = "doce de leite";
+
+    let pedido2 = window.document.querySelector("p#pizzaDoce")
+
+    pedido2.innerHTML = `E também uma <u>${pizza["tamanho"]}</u> de <u>${pizza["sabor"]}</u> com borda de <u>${pizza["borda"]}</u>.`
+ }
