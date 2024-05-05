9c242a2df712d175ed40ecada4e20f5bd8744810
thaisiporto <thaisi.porto@gmail.com>
2024-05-05 08:58:31 -0300

    Update scripts-objetos.js

--- scripts/scripts-objetos.js
+++ scripts/scripts-objetos.js
@@ -20,8 +20,8 @@ function pizDoc() {
     pedido2.innerHTML = `E também uma <u>${pizza["tamanho"]}</u> de <u>${pizza["sabor"]}</u> com borda de <u>${pizza["borda"]}</u>.`
  }
 
- function pizzaPedido() {
-    let pizza = {
+function pizzaPedido() {
+   let pizza = {
         sabor: "calabresa",
         tamanho: "grande",
         borda: "catupiry",
@@ -30,32 +30,59 @@ function pizDoc() {
         }
     }
 
-    let pedido3 = window.document.querySelector("p#pizzaPedido");
+   let pedido3 = window.document.querySelector("p#pizzaPedido");
         
-    pedido3.innerHTML = pizza.fazerPedido();        
- }
+   pedido3.innerHTML = pizza.fazerPedido();        
+}
 
  // MATH
 
- function mRound() {
-    let roundNumber = Math.round(9.7);
-    let mathRound = window.document.querySelector("span#mRound");
+function mRound() {
+   let numberRound1 = window.document.querySelector("input#numberRound1");
+   let numberRound2 = window.document.querySelector("input#numberRound2");
+   let stringFloatRound = `${numberRound1.value}.${numberRound2.value}`
+   let numberFloatRound = Number(stringFloatRound);
+   let roundNumber = Math.round(numberFloatRound);
+   let mathRound = window.document.querySelector("span#mRound");
 
-    mathRound.innerHTML = "&nbsp; &nbsp" + roundNumber;
- }
+   mathRound.innerHTML = "&nbsp; &nbsp" + roundNumber;
+
+   let roundInteiro = window.document.querySelector("span#roundInteiro");
+   roundInteiro.innerHTML = numberRound1.value;
+   let roundDecimal = window.document.querySelector("span#roundDecimal");
+   roundDecimal.innerHTML = numberRound2.value;
+}
 
  function mCeil() {
-    let ceilNumber = Math.ceil(9.1);
-    let mathCeil = window.document.querySelector("span#mCeil");
+   let numberCeil1 = window.document.querySelector("input#numberCeil1");
+   let numberCeil2 = window.document.querySelector("input#numberCeil2");
+   let stringFloatCeil = `${numberCeil1.value}.${numberCeil2.value}`
+   let numberFloatCeil = Number(stringFloatCeil);
+   let ceilNumber = Math.ceil(numberFloatCeil);
+   let mathCeil = window.document.querySelector("span#mCeil");
 
-    mathCeil.innerHTML = "&nbsp; &nbsp" + ceilNumber;
+   mathCeil.innerHTML = "&nbsp; &nbsp" + ceilNumber;
+
+   let ceilInteiro = window.document.querySelector("span#ceilInteiro");
+   ceilInteiro.innerHTML = numberCeil1.value;
+   let ceilDecimal = window.document.querySelector("span#ceilDecimal");
+   ceilDecimal.innerHTML = numberCeil2.value;
  }
 
  function mFloor() {
-    let floorNumber = Math.floor(10.1);
-    let mathFloor = window.document.querySelector("span#mFloor");
+   let numberFloor1 = window.document.querySelector("input#numberFloor1");
+   let numberFloor2 = window.document.querySelector("input#numberFloor2");
+   let stringFloatFloor = `${numberFloor1.value}.${numberFloor2.value}`
+   let numberFloatFloor = Number(stringFloatFloor);
+   let floorNumber = Math.floor(numberFloatFloor);
+   let mathFloor = window.document.querySelector("span#mFloor");
 
-    mathFloor.innerHTML = "&nbsp; &nbsp" + floorNumber;
+   mathFloor.innerHTML = "&nbsp; &nbsp" + floorNumber;
+
+   let floorInteiro = window.document.querySelector("span#floorInteiro");
+   floorInteiro.innerHTML = numberFloor1.value;
+   let floorDecimal = window.document.querySelector("span#floorDecimal");
+   floorDecimal.innerHTML = numberFloor2.value;
  }
 
  function mPow() {
@@ -75,7 +102,7 @@ function pizDoc() {
    if (Number.isInteger(sqrtNumber)) {
       sqrtNumber = sqrtNumber.toFixed(0);
    } else {
-      sqrtNumber = sqrtNumber.toFixed(1);
+      sqrtNumber = sqrtNumber.toFixed(2);
    }
 
    mathSqrt.innerHTML = "&nbsp; &nbsp" + sqrtNumber;
@@ -90,7 +117,7 @@ function pizDoc() {
 
 
  function mRandomInteiro() {
-    let randomIntegerNumber = Math.round(Math.random() * 10) + 1;
+    let randomIntegerNumber = Math.ceil(Math.random() * 10);
     let mathRandomInteiro = window.document.querySelector("span#mRandomInteiro");
 
     mathRandomInteiro.innerHTML = "&nbsp; &nbsp" + randomIntegerNumber;
@@ -105,13 +132,21 @@ function pizDoc() {
 
 function secondDate() {
    let segundaData = window.document.querySelector("span#secondDate");
-   let numberAno = window.document.querySelector("input#numberAno");
-   let numberMes = window.document.querySelector("input#numberMes");
-   let numberDia = window.document.querySelector("input#numberDia");
-   // Convert
-   let data2 = new Date(`${numberAno.value}-${numberMes.value}-${numberDia.value}`)
+   let numberAno = window.document.querySelector("span#numberAno");
+   let numberMes = window.document.querySelector("span#numberMes");
+   let numberDia = window.document.querySelector("span#numberDia");
+   var idata = window.document.querySelector("input#idata");
 
+   // Convert
+   let data2 = new Date(idata.value)
    segundaData.innerHTML = data2;
+
+   let ano = data2.getFullYear();
+   let mes = data2.getMonth()+1;
+   let dia = data2.getDate()+1;
+   numberAno.innerHTML = `<span style="color:#D94C1A;"><strong>${ano}</strong></span>`;
+   numberMes.innerHTML = `<span style="color:#D94C1A;"><strong>${mes}</strong></span>`;
+   numberDia.innerHTML = `<span style="color:#D94C1A;"><strong>${dia}</strong></span>`;
  }
 
  function diaDoMes() {
@@ -119,5 +154,8 @@ function secondDate() {
    const data3 = new Date();
    let dia = data3.getDate();
 
-   monthDay.innerHTML = `Hoje é dia <span style="color:#d3a10b; font-size:1em;">${dia}</span>!`;
+   monthDay.innerHTML = `Hoje é dia <span style="color:#d3a10b;">${dia}</span>!`;
  }
+
+
+
