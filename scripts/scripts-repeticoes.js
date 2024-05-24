70d59298ea82fa4fc404adfe35ff0c9d1da52ccf
thaisiporto <thaisi.porto@gmail.com>
2024-05-24 04:29:24 -0300

    Update scripts-repeticoes.js

--- scripts/scripts-repeticoes.js
+++ scripts/scripts-repeticoes.js
@@ -1,10 +1,19 @@
 function whileRep() {
-    let passos = 1;
-    while (passos <= 4) {
+    let numeroPasso = 1;
+    while (numeroPasso <= 4) {
         let novaTag = document.createElement("span");
         let repeticao = window.document.querySelector("p#whileRep").appendChild(novaTag);
 
-        repeticao.innerHTML = "Passo " + passos + "<br>";
-        passos++;
+        repeticao.innerHTML = `Passo &nbsp;<strong style="color:#172a3a;">${numeroPasso}</strong><br>`;
+        numeroPasso++;
     }
+}
+
+function doWhileRep() {
+    let numeroPassoFalso = 1;
+    do {
+        let repeticaoFalsa = window.document.querySelector("p#doWhileRep");
+        repeticaoFalsa.innerHTML = `Passo &nbsp;<strong style="color:#172a3a;">${numeroPassoFalso}</strong><br>`;
+        numeroPassoFalso++;
+    } while (numeroPassoFalso === 0)
 }
