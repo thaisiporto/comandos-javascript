f9cd0c06a02853e54d81b40f246f4c433230c923
thaisiporto <thaisi.porto@gmail.com>
2024-05-25 04:50:16 -0300

    Update scripts-repeticoes.js

--- scripts/scripts-repeticoes.js
+++ scripts/scripts-repeticoes.js
@@ -1,19 +1,38 @@
-function whileRep() {
-    let numeroPasso = 1;
-    while (numeroPasso <= 4) {
-        let novaTag = document.createElement("span");
-        let repeticao = window.document.querySelector("p#whileRep").appendChild(novaTag);
+function seqRep() {
+    let novaTagSeq = document.createElement("span");
+    let repeticaoSeq = window.document.querySelector("p#seqRep").appendChild(novaTagSeq);
 
-        repeticao.innerHTML = `Passo &nbsp;<strong style="color:#172a3a;">${numeroPasso}</strong><br>`;
-        numeroPasso++;
+    repeticaoSeq.innerHTML += `Erro&nbsp;<strong style="color:#172a3a;"> 1</strong><br>`;
+    repeticaoSeq.innerHTML += `Erro&nbsp;<strong style="color:#172a3a;"> 2</strong><br>`;
+    repeticaoSeq.innerHTML += `Erro&nbsp;<strong style="color:#172a3a;"> 3</strong><br>`;
+    repeticaoSeq.innerHTML += `Erro&nbsp;<strong style="color:#172a3a;"> 4</strong><br>`;
+}
+
+function whileRep() {
+    let numeroPassoWhile = 1;
+    while (numeroPassoWhile <= 4) {
+        let novaTagWhile = document.createElement("span");
+        let repeticaoWhile = window.document.querySelector("p#whileRep").appendChild(novaTagWhile);
+
+        repeticaoWhile.innerHTML = `Passo &nbsp;<strong style="color:#172a3a;">${numeroPassoWhile}</strong><br>`;
+        numeroPassoWhile++;
     }
 }
 
 function doWhileRep() {
-    let numeroPassoFalso = 1;
+    let numeroTarefaDo = 1;
     do {
-        let repeticaoFalsa = window.document.querySelector("p#doWhileRep");
-        repeticaoFalsa.innerHTML = `Passo &nbsp;<strong style="color:#172a3a;">${numeroPassoFalso}</strong><br>`;
-        numeroPassoFalso++;
-    } while (numeroPassoFalso === 0)
+        let repeticaoDo = window.document.querySelector("p#doWhileRep");
+        repeticaoDo.innerHTML = `Tarefa &nbsp;<strong style="color:#172a3a;">${numeroTarefaDo}</strong><br>`;
+        numeroTarefaDo++;
+    } while (numeroTarefaDo === 0)
+}
+
+function forRep() {
+    for (let numeroMissaoFor = 1; numeroMissaoFor <= 4; numeroMissaoFor++) {
+        let novaTagFor = document.createElement("span");
+        let repeticaoFor = window.document.querySelector("p#forRep").appendChild(novaTagFor);
+
+        repeticaoFor.innerHTML = `Missão &nbsp;<strong style="color:#172a3a;">${numeroMissaoFor}</strong><br>`;
+    }
 }
